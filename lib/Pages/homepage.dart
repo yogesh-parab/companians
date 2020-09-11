5c46444151e2c3f89b366e339e4a5857956dcd12
yogesh-parab <yogesh.np@somaiya.edu>
2020-09-12 02:19:02 +0530

    u1

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -5,8 +5,82 @@ import 'package:Final/bloc.navigation_bloc/navigation_bloc.dart';
 class HomePage extends StatelessWidget with NavigationStates {
   @override
   Widget build(BuildContext context) {
-    return Center(child:Text("HomePage",style: TextStyle(fontSize: 40),)
-      
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xffBC0253),
+        child: Icon(Icons.add),
+        shape: CircleBorder(side: BorderSide(color: Colors.white, width: 3.0)),
+        onPressed: null,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 32,
+        backgroundColor: Color(0xffBC0253),
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.pink[100],
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            title: Text('shop'),
+            backgroundColor: Colors.white,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            title: Text('maps'),
+            backgroundColor: Colors.white,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pets),
+            title: Text('pets'),
+            backgroundColor: Color(0xffBC0253),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help),
+            title: Text('tips'),
+            backgroundColor: Color(0xffBC0253),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Image(
+            image: NetworkImage(
+                "https://images.pexels.com/photos/220938/pexels-photo-220938.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.6,
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
+                      ),
+                      child: Image(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.7,
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/images/bg.png'),
+                      ),
+                    )
+                  ],
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(40.0),
+                      topLeft: Radius.circular(40.0),
+                    ))),
+          )
+        ],
+      ),
     );
   }
 }
